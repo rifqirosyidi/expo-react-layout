0c5a7646ca19a04f71bea380942f3ebb5ee357f0
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-17 17:36:51 +0700

    App Layout

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
 
 export default function App() {
 
@@ -9,15 +9,25 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.textOne}>Hello, to React Native :D</Text>
-      <Text>Whatever We Want!</Text>
-      <Image 
-        style={{ width: 100, height: 100 }} 
-        source={require('./assets/img-react.png')} />
-      <Image
-        style={{ width:100, height:100}}
-        source={{ uri: 'https://picsum.photos/200/200'}} />
-      <Button title="Click Me" color="#0F111A" onPress={this.pressed} />
+      <StatusBar 
+        hidden={true}
+        barStyle={"dark-content"}
+        networkActivityIndicatorVisible={true} 
+      />
+      <View style={styles.ic1}>
+        <Text>One</Text>
+      </View>
+      <View style={styles.icRow} >
+        <View style={styles.ic2}>
+          <Text>Two</Text>
+        </View>
+        <View style={styles.ic3}>
+          <Text>Three</Text>
+        </View>
+      </View>
+      <View style={styles.ic4}>
+        <Text>Four</Text>
+      </View>
     </View>
   );
 }
@@ -25,11 +35,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    // LayoutDirection Orientation (column, row)
+    flexDirection: "column",
+    backgroundColor: '#FFFFFF',
   },
-  textOne: {
-    color: '#ff0000'
+  ic1: {
+    backgroundColor: "#4444EE",
+    flex: 1,
+    padding: 20,
+    color: "#FFF",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+    ic2: {
+      backgroundColor: "#FFFF47",
+      flex: 3,
+      padding: 20
+    },
+    ic3: {
+      backgroundColor: "#126947",
+      flex: 1,
+      padding: 20
+  },
+  ic4: {
+    backgroundColor: "#EEAA47",
+    flex: 1,
+    padding: 20
+  },
+  icRow: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 20,
   }
 });
